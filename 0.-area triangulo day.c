97f7149fe39c27f022cef074d1df3c13fb161c03
Dayana-23 <movd100116mmcrrya4@lake.edu.mx>
2025-10-21 12:48:02 -0600

    0.-Calcular area del triangulo

---
+++ 0.-area triangulo day.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+main(){
+float altura;
+float base;
+float area;
+
+printf("Ingresa el valor de la altura ");
+scanf("%f",&altura);
+
+printf("Ingresa el valor de la base");
+scanf("%f",&base);
+
+area=(base*altura) / 2;
+
+printf("El area del triangulo es:%.2f \n",area);
+}
+
