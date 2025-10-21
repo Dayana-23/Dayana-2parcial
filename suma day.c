a705ee852c1e36798a0b6e4e079de1eb0b3ec7d5
Dayana-23 <movd100116mmcrrya4@lake.edu.mx>
2025-10-21 13:04:08 -0600

    1 day sumas 3 numeros

---
+++ suma day.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+main(){
+float n1;
+float n2;
+float n3;
+float suma;
+
+printf("Ingresa el valor de el n1");
+scanf( "%f",&n1);
+
+printf("Ingresa el valor de el n2");
+scanf("%f",&n2);
+
+printf("Ingresa el valor de el n1");
+scanf("%f",&n3);
+suma = n1 + n2 + n3 ;
+
+printf("El resultado de la suma es:%2f",suma);
+}
