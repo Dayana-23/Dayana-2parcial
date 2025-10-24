84af291762ba1eb82c763b36f6ed0a77412fa853
Dayana-23 <movd100116mmcrrya4@lake.edu.mx>
2025-10-24 09:52:18 -0600

    conversion de cm a pulgadas

---
+++ pl a cm dayana.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+main(){
+
+float cm;
+float pl;
+
+printf("ingresa los centimetros:\n");
+scanf("%f",&cm);
+
+pl=cm*0.3937;
+printf("Tu distancia en pulgadas es: \n%f" ,pl);
+
+}
