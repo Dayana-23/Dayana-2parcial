4136c6d54d5872f7e1e93ebba951dc8dba7e41bd
Dayana-23 <movd100116mmcrrya4@lake.edu.mx>
2025-11-06 18:15:25 -0600

    243

---
+++ main (1).c
@@ -0,0 +1,19 @@
+/******************************************************************************
+
+                            Online C Compiler.
+                Code, Compile, Run and Debug C program online.
+Write your code in this editor and press "Run" button to compile and execute it.
+
+*******************************************************************************/
+
+#include <stdio.h>
+
+int main()
+{
+    printf("Hello World");
+
+    int resultado;
+    resultado = 243 * 243;
+    printf("El cuadrado de 243 es: %d", resultado);
+    return 0;
+}
