9000d14994f33771c7d8683ec5333c0d7114bee0
Dayana-23 <movd100116mmcrrya4@lake.edu.mx>
2025-11-11 13:04:01 -0600

    multiplicacion 243
    243

--- main (1).c
+++ main (1).c
@@ -1,19 +1,13 @@
-/******************************************************************************
 
-                            Online C Compiler.
-                Code, Compile, Run and Debug C program online.
-Write your code in this editor and press "Run" button to compile and execute it.
-
-*******************************************************************************/
 
 #include <stdio.h>
 
 int main()
 {
-    printf("Hello World");
+  
 
     int resultado;
     resultado = 243 * 243;
     printf("El cuadrado de 243 es: %d", resultado);
-    return 0;
+\\Solo multiplique los numeros y utilize printf para mostrar el resultado
 }
