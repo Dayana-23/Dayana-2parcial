f523041aafc01ba7336d8ae847124a0544391b2b
Dayana-23 <movd100116mmcrrya4@lake.edu.mx>
2025-11-11 13:01:07 -0600

    Ecuaciones de segundo grado :)

---
+++ ecuaciones 2 day.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+#include <Math.h>
+main()
+{
+    float raizinterior;
+    float raizfinal;
+    float a,b,c,x1,x2;
+
+    printf("ingresa el valor de a");
+    scanf("%f",&a);
+    printf("ingresa el valor de b");
+    scanf("%f",&b);
+    printf("ingresa el valor de c");
+    scanf("%f",&c);
+
+
+    raizinterior=(b*b)-(4*a*c);
+    raizfinal=sqrt(raizinterior);
+    x1=((-b)+raizfinal)/(2*a);
+
+    printf("x1 %f",x1);
+    printf("\n");
+
+    raizinterior=(b*b)-(4*a*c);
+    raizfinal=sqrt(raizinterior);
+    x2=((-b)-raizfinal)/(2*a);
+
+    printf("x2 %f",x2);
+    printf("\n");
+\\ Utilize la formula general para hacer mis calculos y leugo al intertar con el codigo verificar que los resultados fueran correctos
+}
