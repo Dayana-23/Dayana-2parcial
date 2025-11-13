81e4040d003989f64837f0b1374ca0b7209c2800
Dayana-23 <movd100116mmcrrya4@lake.edu.mx>
2025-11-12 19:20:34 -0600

    Cero o no

---
+++ main (2).c
@@ -0,0 +1,18 @@
+
+
+#include <stdio.h>
+
+int main()
+{
+int n;
+printf("Numero: ");
+scanf("%d", &n);
+if (n==0)
+printf("Es cero");
+else 
+printf("No es cero");
+\\ Lo que hice fue simple es solo una condicionante, se cumple o no 
+
+
+    }
+ 
