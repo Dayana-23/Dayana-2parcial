b6dd5b7a5ca75542feb605a0befdd44644e9a50d
Dayana-23 <movd100116mmcrrya4@lake.edu.mx>
2025-11-14 09:22:17 -0600

    243

--- main (1).c
+++ main (1).c
@@ -4,10 +4,8 @@
 
 int main()
 {
-  
-
     int resultado;
     resultado = 243 * 243;
     printf("El cuadrado de 243 es: %d", resultado);
-\\Solo multiplique los numeros y utilize printf para mostrar el resultado
+//Solo multiplique los numeros y utilize printf para mostrar el resultado
 }
